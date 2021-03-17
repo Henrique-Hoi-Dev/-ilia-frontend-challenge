6a2c5a58e65f874563b1fc6b98caf1011603c4a1
Henrique Hoinacki <riqueah@gmail.com>
2021-03-17 16:38:18 -0300

    information page

--- challenge-vue-ilia/src/views/StatesPokemon.vue
+++ challenge-vue-ilia/src/views/StatesPokemon.vue
@@ -1,21 +1,44 @@
 <template>
- <div>
-    <h1> {{ name }} </h1>    
-    <pokemon-info >
-    </pokemon-info>
- </div>     
+ <div class="pokemon-info" >
+     <h1>{{ name }} </h1>
+    <hr> 
+   <div v-for="pokemon in data" :key="pokemon.id"> 
+    <p >{{ pokemon.name }} -- {{ pokemon.id }}</p>
+     <img :src="pokemon.imageUrl" :alt="pokemon.name" >
+   </div>
+    <h2>Veio para page</h2> 
+    <b-modal>
+      
+    </b-modal>
+  </div> 
 </template>
 
 <script>
 import mixinPokemon from '../mixinPokemon';
-import PokemonInfo from '@/components/PokemonInfo.vue'
 
 export default {
-  props: {  },
-  components: { PokemonInfo },
-  name: 'StatesPokemon',
-  mixins: [mixinPokemon]
-}
+  props: ['id'],
+  mixins: [mixinPokemon],
+  data() {
+    return {     
+      data: {}
+    }
+  },
+  beforeMount(){
+    this.getName();
+  },
+  methods: { 
+    async getName(){
+      const idP = this.$route.params.id;
+
+      const res = await fetch(`https://api.pokemontcg.io/v1/cards/${idP}`);
+      const data = await res.json();
+      this.data = data;
+
+      console.log(idP.name)
+    }
+  }
+}  
 </script>
 
 <style>
